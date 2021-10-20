54a71413b28729ea19a77816103f35f4a5ef7d58
blueweaver <brandonweaver2011@hotmail.com>
2021-10-19 21:30:32 -0700

    add R script to make the dataframes

---
+++ importDataset.R
@@ -0,0 +1,3 @@
+library("readxl")
+allStatesDataFrame <- read_xlsx("This is Statistics Fall Data Challenge 2021 Dataset_ Food Access Research Atlas Data 2019-ASAFDC 2021.xlsx", 3)
+californiaDataFrame <- read_xlsx("This is Statistics Fall Data Challenge 2021 Dataset_ Food Access Research Atlas Data 2019-ASAFDC 2021.xlsx", 8)
